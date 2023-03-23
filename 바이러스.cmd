c1396c6322f1f0e4de60f8c68e84d80935f93780
esp32-urobot <urobot2011@gmail.com>
2023-03-23 13:27:34 +0900

    Update 바이러스.cmd

--- 바이러스.cmd
+++ 바이러스.cmd
@@ -6,11 +6,14 @@ color 17
  
 taskkill /f /im explorer.exe
 cls
-echo ����� ġ������ �Ǽ��ڵ忡 ���� �Ǿ����ϴ�.
+echo https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=skdh88&logNo=221046472967
+cls
+
+echo 당신은 치명적인 악성코드에 감염 되었습니다.
  
-echo ���� ���: ��й�ȣ�� �ּ���.
+echo 복구 방법: 비밀번호를 주세요.
  
-set /p password=��й�ȣ:
+set /p password=비밀번호:
 if %password%==1234(
 goto case2
 ) else (
@@ -19,18 +22,18 @@ goto case1
  
 :case1
 cls
-echo �������� ��й�ȣ�� Ʋ�Ƚ��ϴ�!! ��ǻ�� �� �����߸����� ��������
+echo ㅋㅋㅋㅋ 비밀번호가 틀렸습니다!! 컴퓨터 잘 망가뜨릴께요 ㅋㅋㅋㅋ
 timeout /t 10
-scho �̷�, ��� ������ �޾ҽ��ϴ�. �ФФ� ��ǻ�͸� �����߸��� ����׿� �ФФ�
+scho 이런, 방금 명령을 받았습니다. ㅠㅠㅠ 컴퓨터를 망가뜨리지 말라네요 ㅠㅠㅠ
 timeout /t 5
 start C:\Windows\explorer.exe
-echo ������
-echo ������
-echo ������
+echo 흑흑흑
+echo 흑흑흑
+echo 흑흑흑
 pause
  
 :case2
 cls
-echo ��й�ȣ�� �½��ϴ�. ��ǻ�͸� �����߸��Ⱑ ��ҵǾ����ϴ�. �ФФ�
+echo 비밀번호가 맞습니다. 컴퓨터를 망가뜨리기가 취소되었습니다. ㅠㅠㅠ
 start C:\Windows\explorer.exe
 pause
